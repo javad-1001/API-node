d0c09792ad2e206e8ee856d8f0126329f6b2123a
Javad Babaei <106420605+javad-1001@users.noreply.github.com>
2024-05-26 16:24:55 +0330

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express =  require('express') ;
-const bodyParser =  require('body-parser') ;
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 
+app.use(cors())
 
 
 // query for add user
@@ -15,61 +17,87 @@ app.use(bodyParser.json());
 
 
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 const e = require('express');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'javad',
-  password : 'javad',
-  database : 'mehran_db'
+  host: 'localhost',
+  user: 'javad',
+  password: 'javad',
+  database: 'behdood_db'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   // connected! (unless `err` is set)
 });
 
 
-app.post('/api/addUser',
-    (req, res) => {
-      const { strPassword, iMobileNumber, strName, strFamily, iRole, bActive } = req.body;
+app.get('/getAllProducts', (req, res) => {
+  const query = 'SELECT * FROM products';
 
-      const insertQuery = `INSERT INTO users (strPassword, iMobileNumber, strName, strFamily, iRole, bActive) VALUES (?, ?, ?, ?, ?, ?)`;
-      const values = [strPassword, iMobileNumber, strName, strFamily, iRole, bActive];
-    
-      connection.query(insertQuery, values, (err, result) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('Failed to insert user');
-        }
-        
-        res.status(201).send('User inserted successfully');
-      });
-      
-    });
+  connection.query(query, (error, results) => {
+    if (error) {
+      res.status(500).json({ error: error.message });
+      return;
+    }
 
-
-
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+    res.json(results);
   });
+});
 
-  app.get('/', (req, res) => {
-    console.log('[GET ROUTE]');
-    res.send('HELLO FROM HOMEPAGE');
-})
+app.post('/login_admin', (req, res) => {
+  const { strUsername, strPassword } = req.body;
 
+  const query = `SELECT * FROM user_admin WHERE strUsername = ? AND strPassword = ?`;
 
-// app.post('/api/todos',
-//     (req, res) => {
-//         const { username, password } = req.body;
-//         const { authorization } = req.headers;
-//         res.send(
-//             {
-//                 username,
-//                 password,
-//                 message: 'done successfully!' ,
-//                 authorization,
-//             });
+  connection.query(query, [strUsername, strPassword], (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Database error' });
+      return;
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ result: true, message: 'Login successful' });
+    } else {
+      res.status(401).json({ result: false, error: 'Invalid credentials' });
+    }
+  });
+});
+
+// app.post('/api/addUser',
+//   (req, res) => {
+//     const { strPassword, iMobileNumber, strName, strFamily, iRole, bActive } = req.body;
+
+//     const insertQuery = `INSERT INTO users (strPassword, iMobileNumber, strName, strFamily, iRole, bActive) VALUES (?, ?, ?, ?, ?, ?)`;
+//     const values = [strPassword, iMobileNumber, strName, strFamily, iRole, bActive];
+
+//     connection.query(insertQuery, values, (err, result) => {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).send('Failed to insert user');
+//       }
+
+//       res.status(201).send('User inserted successfully');
 //     });
+
+//   });
+
+
+
+// app.delete('/api/users_Delete/:id', (req, res) => {
+//   const { id } = req.params;
+//   const DELETE_QUERY = `DELETE FROM users WHERE id = ${id}`;
+
+//   db.query(DELETE_QUERY, (err, result) => {
+//     if (err) {
+//       res.status(500).send(err.message);
+//     } else {
+//       res.send(`Deleted ${result.affectedRows} row(s)`);
+//     }
+//   });
+// });
+
+
+
+
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
